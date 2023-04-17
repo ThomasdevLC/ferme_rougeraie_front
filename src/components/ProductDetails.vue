6817934c2f635bbd5b3761d5d473e9f67a9c86f9
ThomasdevLC <thomas.lecam@gmail.com>
2023-04-17 12:43:50 +0200

    modal click+ style

--- src/components/ProductDetails.vue
+++ src/components/ProductDetails.vue
@@ -2,7 +2,12 @@
   <div class="task">
     <h3>{{ product.title }}</h3>
     <div class="icons">
-      <i class="material-icons">edit</i>
+      <div class="overlay" v-if="showModal" @click="closeModal"></div>
+
+      <div class="modal" v-if="showModal">Hello modal</div>
+
+      <i class="material-icons" @click="openModal">edit</i>
+
       <i class="material-icons" @click="productStore.deleteProduct(product.id)"
         >delete</i
       >
@@ -30,6 +35,20 @@ import { useProductStore } from "../stores/ProductStore";
 export default {
   props: ["product"],
 
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+
   setup() {
     const productStore = useProductStore();
 
@@ -38,4 +57,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  z-index: 999;
+}
+
+.modal {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999;
+  background: white;
+}
+</style>
