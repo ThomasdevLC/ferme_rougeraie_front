45e6a4c24f35f7a011f505a4408471580ec3240f
ThomasdevLC <thomas.lecam@gmail.com>
2023-04-19 16:03:27 +0200

    style form modif

--- src/components/EditModal.vue
+++ src/components/EditModal.vue
@@ -121,4 +121,28 @@ export default {
   object-fit: cover;
   border-radius: 50%;
 }
+
+form {
+  max-width: 400px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: 3fr 1fr;
+  gap: 10px;
+}
+form button {
+  background: #ffd859;
+  border: 0;
+  padding: 10px;
+  font-family: "Poppins";
+  border-radius: 6px;
+  cursor: pointer;
+  font-size: 1em;
+}
+form input {
+  border: 0;
+  padding: 10px;
+  border-radius: 6px;
+  color: #555;
+  font-size: 1em;
+}
 </style>
