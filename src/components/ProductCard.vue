470206d7723006177292ec52e15ec384ff264d3f
ThomasdevLC <thomas.lecam@gmail.com>
2023-04-24 17:37:31 +0200

    productcard component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="card-container">
+    <div v-if="isLoading" class="card skeleton">
+      <div class="skeleton-img"></div>
+      <div class="skeleton-text">
+        <div class="skeleton-line"></div>
+        <div class="skeleton-line"></div>
+        <div class="skeleton-line"></div>
+      </div>
+    </div>
+    <div v-else class="card">
+      <img src="https://via.placeholder.com/150" alt="product image" />
+      <h3>Product Name</h3>
+      <p>Description of the product goes here.</p>
+      <button>Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isLoading: true, // set the initial value to true to show the skeleton
+    };
+  },
+  mounted() {
+    // simulate loading for 2 seconds
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  },
+};
+</script>
+
+<style scoped>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
+.card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  flex: 1 0 300px;
+}
+
+.skeleton {
+  position: relative;
+  /* background-color: #f5f5f5; */
+  overflow: hidden;
+}
+
+.skeleton::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -150px;
+  width: 150px;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.6);
+  animation: skeleton-loading 1.5s ease-in-out infinite;
+}
+
+.skeleton-img {
+  width: 150px;
+  height: 150px;
+  margin: 0 auto 20px;
+  background-color: #ccc;
+}
+
+.skeleton-text {
+  width: 100%;
+}
+
+.skeleton-line {
+  height: 20px;
+  margin-bottom: 10px;
+  background-color: #ccc;
+}
+
+@keyframes skeleton-loading {
+  0% {
+    left: -150px;
+  }
+  100% {
+    left: 100%;
+  }
+}
+</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>Products</h1>
+  <div class="background-image">
+    <div class="card-container">
+      <ProductCard />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import background from "../assets/images/background.png";
+import ProductCard from "../components/ProductCard.vue";
+
+export default {
+  components: { ProductCard },
+
+  data() {
+    return { image: background };
+  },
+};
 </script>
 
-<style></style>
+<style>
+.background-image {
+  background-image: url("../assets/images/background.png");
+  background-repeat: repeat-y;
+  background-size: 100% 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.card-container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+}
+</style>
