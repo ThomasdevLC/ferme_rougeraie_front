782b25339cd8a2327b4fd24d3f28b00d385b9688
ThomasdevLC <thomas.lecam@gmail.com>
2023-06-09 14:56:29 +0200

    animation intro

--- src/views/IntroView.vue
+++ src/views/IntroView.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="container">
     <div class="vegie veg-1"><img class="header__logo" :src="tomatoe" /></div>
-    <div class="vegie veg-2"><img class="header__logo" :src="butternut" /></div>
-
+    <div class="vegie veg-2">
+      <img class="header__logo" :src="butternut" />
+    </div>
     <div class="vegie veg-3"><img class="header__logo" :src="pumpkin" /></div>
-
-    <div class="vegie veg-4"><img class="header__logo" :src="carrot" /></div>
-
+    <div class="vegie veg-4"><img class="header__logo" :src="turnip" /></div>
     <div class="vegie veg-5"><img class="header__logo" :src="carrot" /></div>
-
-    <div class="vegie veg-6"><img class="header__logo" :src="butternut" /></div>
+    <div class="vegie veg-6">
+      <img class="header__logo" :src="butternut" />
+    </div>
     <div class="vegie veg-7"><img class="header__logo" :src="pumpkin" /></div>
-    <div class="vegie veg-8"><img class="header__logo" :src="butternut" /></div>
-    <div class="vegie veg-9"><img class="header__logo" :src="carrot" /></div>
-    <div class="vegie veg-10"><img class="header__logo" :src="leeks" /></div>
+    <div class="vegie veg-8"><img class="header__logo" :src="tomatoe" /></div>
+    <div class="vegie veg-9"><img class="header__logo" :src="turnip" /></div>
+    <div class="vegie veg-10">
+      <img class="header__logo" :src="carrot" />
+    </div>
     <div class="text"><img class="text" :src="text" /></div>
   </div>
 </template>
@@ -22,8 +24,9 @@
 import tomatoe from "../assets/images/tomatoe.png";
 import butternut from "../assets/images/butternut.png";
 import pumpkin from "../assets/images/pumpkin.png";
-import leeks from "../assets/images/leeks.png";
+import turnip from "../assets/images/turnip.png";
 import carrot from "../assets/images/carrot.png";
+import radish from "../assets/images/radish.png";
 import text from "../assets/images/text.svg";
 export default {
   data() {
@@ -31,17 +34,18 @@ export default {
       tomatoe: tomatoe,
       butternut: butternut,
       pumpkin: pumpkin,
-      leeks: leeks,
+      turnip: turnip,
       carrot: carrot,
+      radish: radish,
       text: text,
     };
   },
 
-  mounted() {
-    setTimeout(() => {
-      this.$router.push({ name: "AboutView" });
-    }, 5000);
-  },
+  // mounted() {
+  //   setTimeout(() => {
+  //     this.$router.push({ name: "AboutView" });
+  //   }, 5000);
+  // },
 };
 </script>
 <style scoped>
@@ -65,10 +69,10 @@ export default {
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr 1fr;
+  grid-template-rows: repeat(3, 1fr);
   grid-auto-columns: 1fr;
   gap: 6px 6px;
-  grid-auto-flow: row;
+  /* grid-auto-flow: row; */
   grid-template-areas:
     "veg-1 veg-2 veg-3 veg-4"
     "veg-5 text text veg-6"
@@ -85,10 +89,14 @@ export default {
 
 .veg-3 {
   grid-area: veg-3;
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1) rotate(6deg);
 }
 
 .veg-4 {
   grid-area: veg-4;
+  transform: rotate(4deg);
+  height: 200px;
 }
 
 .veg-5 {
@@ -97,6 +105,8 @@ export default {
 
 .veg-6 {
   grid-area: veg-6;
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
 }
 
 .veg-7 {
@@ -105,6 +115,8 @@ export default {
 
 .veg-8 {
   grid-area: veg-8;
+  height: 250px;
+  transform: rotate(20deg);
 }
 
 .veg-9 {
@@ -113,11 +125,14 @@ export default {
 
 .veg-10 {
   grid-area: veg-10;
+  -webkit-transform: scaleX(-1);
+  transform: scaleX(-1);
+  height: 280px;
 }
 
 .text {
   grid-area: text;
-  height: 450px;
+  height: 400px;
   margin: auto;
 }
 
