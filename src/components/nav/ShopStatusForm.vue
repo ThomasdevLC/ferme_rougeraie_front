be67e1e6265f71c4758d9602e76268dd1a2465d6
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-09 14:12:26 +0200

    shop status form + function

--- src/components/nav/ShopStatusForm.vue
+++ src/components/nav/ShopStatusForm.vue
@@ -1,18 +1,41 @@
 <template>
-  <form class="shopform" @submit.prevent="handleSubmit">
+  <form class="shopform" @submit.prevent="handleShopStatus">
     <input
       class="shopform__input"
-      id="name"
+      id="message"
       type="text"
       placeholder="message"
-      v-model="name"
+      v-model="message"
     />
     <button class="btn">VALIDER</button>
   </form>
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+import { useProductStore } from "../../stores/ProductStore";
+
+export default {
+  setup() {
+    const productStore = useProductStore();
+    const message = ref("");
+
+    const handleShopStatus = () => {
+      if (message.value.length > 0) {
+        productStore.addProduct({
+          message: message.value,
+        });
+
+        console.log(message.value);
+      }
+    };
+
+    return {
+      message,
+      handleShopStatus,
+    };
+  },
+};
 </script>
 
 <style scoped lang="scss">
@@ -21,9 +44,10 @@ export default {};
   flex-direction: column;
 
   &__input {
+    margin: auto;
     border: 0;
     width: 400px;
-    padding: 14px;
+    padding: 20px;
     font-size: 16px;
     color: var(--gray-4);
     border-radius: 2px;
