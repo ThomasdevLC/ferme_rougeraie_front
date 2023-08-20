00b3a11775ff0a42c9775b6e90f3d600401c0489
ThomasdevLC <thomas.lecam@gmail.com>
2023-08-20 14:30:51 +0200

    about media links

--- src/components/about/AboutInformations.vue
+++ src/components/about/AboutInformations.vue
@@ -6,20 +6,29 @@
         <li>
           <a
             href="https://www.google.com/maps/place/Ferme+de+la+Rougeraie/@47.9659359,-2.5786985,17z/data=!4m14!1m7!3m6!1s0x480fd17bd0d61589:0x327caef9255f9739!2sFerme+de+la+Rougeraie!8m2!3d47.9659323!4d-2.5761236!16s%2Fg%2F11nwwr5hmv!3m5!1s0x480fd17bd0d61589:0x327caef9255f9739!8m2!3d47.9659323!4d-2.5761236!16s%2Fg%2F11nwwr5hmv?authuser=0"
+            target="_blank"
           >
             <img :src="imageGmap" alt="localisation" />
           </a>
         </li>
-        <li><img :src="imageInsta" alt="Instagram" /></li>
         <li>
           <a
-            href="https://www.facebook.com/people/Ferme-de-La-Rougeraie/100077586669860/"
+            href="https://www.instagram.com/fermedelarougeraie/"
+            target="_blank"
+          >
+            <img :src="imageInsta" alt="Instagram" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/people/Ferme-de-La-Rougeraie/100077586669860/  "
+            target="_blank"
           >
             <img :src="imageFaceb" alt="Facebook" />
           </a>
         </li>
         <li>
-          <a href="fermedelarougeraie@gmail.com">
+          <a href="mailto:fermedelarougeraie@gmail.com" target="_blank">
             <img :src="imageEmail" alt="email" />
           </a>
         </li>
